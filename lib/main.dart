1b61d5439da739f41a591baca721e6aac10cc8fa
Dan Levy <danlevy.ca@icloud.com>
2022-06-09 21:10:36 -0400

    Add Firebase to all platform

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:sums_up/themes/dark_theme.dart';
 import 'package:sums_up/themes/light_theme.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 import 'views/home.dart';
 
@@ -13,6 +15,10 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+
     return MaterialApp(
       title: 'Flutter Demo',
       theme: customLightTheme(),
